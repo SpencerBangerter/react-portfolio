f003e1057e8966861a4054fc081754816435be97
Spencer Bangterer <bangerter.spencer@gmail.com>
2020-04-16 21:32:19 -0600

    basic router setup and component creation

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
-import Nav from './components/Nav'
+import {BrowserRouter, Route, Link} from 'react-router-dom'
+import Nav from './components/Nav/Nav'
+import logo from './logo.svg'
+import Projects from './components/Projects/Projects'
+import About from './components/About/About'
+import Skills from './components/Skills/Skills'
 import './App.css';
 
 function App() {
   return (
-    <div>
-      <Nav></Nav>
-    </div>
+    <BrowserRouter>
+    <div className="App">
 
-    );
+      <Route exact path="/" component={Projects} />
+      <Route path="/about" component={About} />
+      <Route path="/skills" component={Skills} />
+      <div className="navigation">
+        <img src={logo} className="logo-image App-logo" alt="Logo Image" />
+        <div className="navigation-sub">
+                                        
+          <Link to="/" className="item">Projects</Link>
+          <Link to="/about" className="item">About</Link>
+          <Link to="/skills" className="item">Skills</Link>
+        </div>
+      </div>
+    </div>
+  </BrowserRouter>    );
 }
 
 export default App;
