21fc5fa49d6f986acc5156c5e7659fa0477c731e
Spencer Bangterer <bangerter.spencer@gmail.com>
2020-04-18 23:37:45 -0600

    final Project page

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,12 +4,16 @@ import Invisitainer from "../Invisitainer/Invisitainer";
 import ProjectCard from "../ProjectCard/ProjectCard";
 import moodImg from "../../images/proj1.png";
 import saltImg from "../../images/saltCityServiceLogo-01.png";
+import quizImg from "../../images/quizzer.png";
+import weatherImg from "../../images/weather.png";
+import passImg from "../../images/password.png";
+import noteImg from "../../images/notes.png"
 export default function Projects() {
   return (
     <Invisitainer
       title="Projects"
       body={
-        <div className="container">
+        <div className="container ml-md-2 ml-sm-1">
           <div className="row">
             <ProjectCard
               cardImg={moodImg}
@@ -26,20 +30,38 @@ export default function Projects() {
               gitLink={"https://github.com/SpencerBangerter/salt-city-services"}
             />
             <ProjectCard
-              cardImg={saltImg}
-              imgId={"saltCityServImg"}
-              projName={"Salt City Services"}
-              liveLink={"http://www.saltcityservice.com/"}
-              gitLink={"https://github.com/SpencerBangerter/salt-city-services"}
+              cardImg={quizImg}
+              imgId={"quizzerImg"}
+              projName={"Code Quizzer"}
+              liveLink={"https://spencerbangerter.github.io/code-quizzer/"}
+              gitLink={"https://github.com/SpencerBangerter/code-quizzer"}
             />
           </div>
           <div className="row">
             <ProjectCard
-              cardImg={saltImg}
-              imgId={"saltCityServImg"}
-              projName={"Salt City Services"}
-              liveLink={"http://www.saltcityservice.com/"}
-              gitLink={"https://github.com/SpencerBangerter/salt-city-services"}
+              cardImg={weatherImg}
+              imgId={"weatherImg"}
+              projName={"Weather Dashboard"}
+              liveLink={"https://spencerbangerter.github.io/weather-dashboard/"}
+              gitLink={"https://github.com/SpencerBangerter/weather-dashboard"}
+            />
+            <ProjectCard
+              cardImg={passImg}
+              imgId={"passImg"}
+              projName={"Password Generator"}
+              liveLink={
+                "https://spencerbangerter.github.io/password-generator/"
+              }
+              gitLink={"https://github.com/SpencerBangerter/password-generator"}
+            />
+            <ProjectCard
+              cardImg={noteImg}
+              imgId={"noteImg"}
+              projName={"Note Taker"}
+              liveLink={
+               "https://sheltered-shore-03476.herokuapp.com/notes"
+              }
+              gitLink={"https://github.com/SpencerBangerter/notes-express"}
             />
           </div>
         </div>
