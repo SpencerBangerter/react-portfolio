d528a37c9fcf3333ebdf570d7a79757c7188a44c
Spencer Bangterer <bangerter.spencer@gmail.com>
2020-04-20 18:39:43 -0600

    Updates

---
+++ precache-manifest.45ae85f44e7945e37dba8dd257754f27.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90befa89e672310ac1e8f93747ec4cea",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "356d9d9a98b0fa1dd877",
+    "url": "/react-portfolio/static/css/main.6f76b8ec.chunk.css"
+  },
+  {
+    "revision": "4241f32342300e064ab2",
+    "url": "/react-portfolio/static/js/2.4ac5df1b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-portfolio/static/js/2.4ac5df1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "356d9d9a98b0fa1dd877",
+    "url": "/react-portfolio/static/js/main.6c0f22aa.chunk.js"
+  },
+  {
+    "revision": "c01136dc6b7e393f307b",
+    "url": "/react-portfolio/static/js/runtime-main.d92c7dfa.js"
+  },
+  {
+    "revision": "c7a4b94f60d60ee8a0d36ff131f47125",
+    "url": "/react-portfolio/static/media/Spencer Bangerter Resume.c7a4b94f.pdf"
+  },
+  {
+    "revision": "cf7e2c43a53ed3a4d6088242b6c55b92",
+    "url": "/react-portfolio/static/media/background.cf7e2c43.jpg"
+  },
+  {
+    "revision": "17b86a9ac2a8892bd19c0811dc107df2",
+    "url": "/react-portfolio/static/media/bio.17b86a9a.jpg"
+  },
+  {
+    "revision": "71e42b5a8d6c0d6628ec8840cc4030e9",
+    "url": "/react-portfolio/static/media/bootstrap.71e42b5a.svg"
+  },
+  {
+    "revision": "d3ad67fa720fd7aee8c322caff911082",
+    "url": "/react-portfolio/static/media/css-3.d3ad67fa.svg"
+  },
+  {
+    "revision": "530516030e26a09ac38473bd5d8eff0e",
+    "url": "/react-portfolio/static/media/express.53051603.svg"
+  },
+  {
+    "revision": "617870e422d4b75087cdcf9ff298032e",
+    "url": "/react-portfolio/static/media/gitimage.617870e4.svg"
+  },
+  {
+    "revision": "24845c5ca600cfab149e84387d826465",
+    "url": "/react-portfolio/static/media/html-5.24845c5c.svg"
+  },
+  {
+    "revision": "87711407412494d529d45ee76167583c",
+    "url": "/react-portfolio/static/media/javascript.87711407.svg"
+  },
+  {
+    "revision": "6ff17bd2dc7d786853da589cb373d553",
+    "url": "/react-portfolio/static/media/jquery.6ff17bd2.svg"
+  },
+  {
+    "revision": "8cc2d3a80c1ebec192c2f29c08333dc6",
+    "url": "/react-portfolio/static/media/linkedin.8cc2d3a8.svg"
+  },
+  {
+    "revision": "fe397f2a174dad6c5f2c36cec0c1ae5e",
+    "url": "/react-portfolio/static/media/materializecss.fe397f2a.svg"
+  },
+  {
+    "revision": "2aa7f7b569fd6e9176f49dd31eaa970e",
+    "url": "/react-portfolio/static/media/mongodb.2aa7f7b5.svg"
+  },
+  {
+    "revision": "dd2a5a358ea682bed45bf98bc842b3ed",
+    "url": "/react-portfolio/static/media/mysql.dd2a5a35.svg"
+  },
+  {
+    "revision": "f434bfbbffc44588572e13e1e113391a",
+    "url": "/react-portfolio/static/media/nodejs.f434bfbb.svg"
+  },
+  {
+    "revision": "253b6e25e0e329a7636401e0b7b31154",
+    "url": "/react-portfolio/static/media/notes.253b6e25.png"
+  },
+  {
+    "revision": "a15614c4312f174651c3dd8d02c15506",
+    "url": "/react-portfolio/static/media/password.a15614c4.png"
+  },
+  {
+    "revision": "7cc694bd7ad6a46d68998ab570d70461",
+    "url": "/react-portfolio/static/media/quizzer.7cc694bd.png"
+  },
+  {
+    "revision": "ab1dbbd07a672d2fc6d1f7236e924671",
+    "url": "/react-portfolio/static/media/saltCityServiceLogo-01.ab1dbbd0.png"
+  },
+  {
+    "revision": "58f2f1fbfb935b5274743895f588488a",
+    "url": "/react-portfolio/static/media/weather.58f2f1fb.png"
+  }
+]);
